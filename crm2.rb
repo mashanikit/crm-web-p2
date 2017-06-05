6cc70d2a1d0624fdbfb46eebd6abdc0a325ddbc0
MASHA <mashanikit@gmail.com>
2017-06-05 14:41:28 -0400

    Finished

--- crm2.rb
+++ crm2.rb
@@ -18,6 +18,17 @@ get '/about' do
   erb :about
 end
 
+get '/contacts/:id' do
+  # params[:id] contains the id from the URL
+
+  @contact = Contact.find(params[:id].to_i)
+    if @contact
+      erb :show_contact
+    else
+      raise Sinatra::NotFound
+    end
+end
+
 after do
   ActiveRecord::Base.connection.close
 end
